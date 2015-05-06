01720bce4d40856b590a2ea8cfe56e3ca5fde7b6
josxsuen <josephinersuen@gmail.com>
2015-05-06 12:12:35 -0700

    Build DeviceModels

--- parser.py
+++ parser.py
@@ -3,7 +3,17 @@
 # On unix12 use python3
 
 import csv
+import os
 # import mysql.connector
+
+def init(table):
+    files[table].write('INSERT INTO %s VALUES\n' % table)
+
+def quote(value):
+    return '"%s"' % value
+
+def values(table, data):
+    files[table].write('(%s),\n' % ','.join(data))
 
 def parse(filename, callback):
     with open(filename) as file:
@@ -18,16 +28,38 @@ def handle_device(data):
     pass
 
 def handle_device_model(data):
-    print(data['Device Name']) # Replace with something useful
+    values('DeviceModels', [
+        quote(data['Device Model']),
+        quote(data['Device Name']),
+        quote(data['Device Type']),
+        quote(data['Carrier'])
+        ])
 
 def handle_email(data):
     pass
 
-def main():
-    parse('data/CP_Account.csv', handle_account)
-    parse('data/CP_Device.csv', handle_device)
-    parse('data/CP_Device_Model.csv', handle_device_model)
-    parse('data/CP_Email_Final.csv', handle_email)
+
+
+
+# Open files
+files = {
+    'DeviceModels': open('DB-build-DeviceModels.sql', 'w+')
+}
+
+for f in files:
+    init(f)
+
+parse('data/CP_Account.csv', handle_account)
+parse('data/CP_Device.csv', handle_device)
+parse('data/CP_Device_Model.csv', handle_device_model)
+parse('data/CP_Email_Final.csv', handle_email)
+
+# Close files
+for f in files:
+    files[f].seek(files[f].tell()-3)
+    files[f].write(';\n')
+    files[f].truncate()
+    files[f].close()
 
 # try:
 #     conn = mysql.connector.connect(user='', password='',
