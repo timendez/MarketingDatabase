1d32086f6baab97d4796bdf39e26a7a70331c517
Josephine Suen <josxsuen@gmail.com>
2015-05-07 12:45:04 -0700

    Parse for Devices, Registrations, and Purchases (not tested), check for duplicate primary keys

--- parser.py
+++ parser.py
@@ -5,59 +5,109 @@
 import csv
 # import mysql.connector
 
-def init(table):
-    files[table].write('INSERT INTO %s VALUES\n' % table)
 
-def quote(value):
-    return '"%s"' % value
 
-def values(table, data):
-    files[table].write('(%s),\n' % ','.join(data))
-
+# Read CSV and send each line to a callback function
 def parse(filename, callback):
     with open(filename) as file:
         reader = csv.DictReader(file, delimiter=',')
         for row in reader:
             callback(row)
 
+# Wrap string value in quotes
+def to_string(value):
+    return '"%s"' % value
+
+def to_date(value, fmt):
+    return 'STR_TO_DATE("%s", "%s")' % (value, fmt)
+
+# If value is not specified, replace with default value
+def if_null(value, default):
+    if len(value) == 0:
+        return default
+    else:
+        return value
+
+# Write dict of values for insert into DB
+def insert_values(table, data):
+    files[table].write('(%s),\n' % ','.join(data))
+
+
+
+
 def handle_account(data):
     pass
 
 def handle_device(data):
-    pass
+    # Devices
+    if data['SerialNumber']:
+        Device = data['SerialNumber']
+        if Device not in pk['Devices']:
+            insert_values('Devices', [
+                to_string(data['SerialNumber']),
+                to_string(data['DeviceModel'])
+                ])
+            pk['Devices'].add(Device)
+
+    # Purchases
+    Purchase = (data['CustomerID'], data['DeviceModel'])
+    if Purchase not in pk['Purchases']:
+        insert_values('Purchases', [
+            to_date(data['PurchaseDate'], '%m/%d/%Y'),
+            to_string(data['PurchaseStoreName']),
+            to_string(data['PurchaseStoreCity']),
+            to_string(data['PurchaseStoreState']),
+            data['Ecomm'],
+            data['CustomerID'],
+            to_string(data['SerialNumber']),
+            to_string(data['DeviceModel'])
+            ])
+        pk['Purchases'].add(Purchase)
+
+    # Registrations
+    Registration = data['RegistrationID']
+    if Registration not in pk['Registrations']:
+        insert_values('Registrations', [
+            to_date(data['RegistrationDate'], '%m/%d/%Y'),
+            data['RegistrationID'],
+            data['SourceID'],
+            to_string(data['SourceName']),
+            to_string(data['DeviceModel']),
+            data['CustomerID'],
+            to_string(data['SerialNumber'])
+            ])
+        pk['Registrations'].add(Registration)
 
 def handle_device_model(data):
-    values('DeviceModels', [
-        quote(data['Device Model']),
-        quote(data['Device Name']),
-        quote(data['Device Type']),
-        quote(data['Carrier'])
-        ])
+    # DeviceModels
+    DeviceModel = data['Device Model']
+    if DeviceModel not in pk['DeviceModels']:
+        insert_values('DeviceModels', [
+            to_string(data['Device Model']),
+            to_string(data['Device Name']),
+            to_string(data['Device Type']),
+            to_string(data['Carrier'])
+            ])
+        pk['DeviceModels'].add(DeviceModel)
 
 def handle_email(data):
     pass
 
 
 
+# List of all tables (in no particular order)
+tables = ['Customers', 'EmailAddresses', 'EmailMessages', 'EventTypes', 'Events', 'Links',
+    'EventLinkLookUp', 'DeviceModels', 'Devices', 'Purchases', 'Registrations']
+# Dict of files where key=table
+files = {}
+# Dict of existing primary keys (as a single value or tuple) for each table
+pk = {}
 
-# Open files, key=TableName
-files = {
-    'Customers'         : open('build/DB-build-Customers.sql', 'w+'),
-    'EmailAddresses'    : open('build/DB-build-EmailAddresses.sql', 'w+'),
-    'EmailMessages'     : open('build/DB-build-EmailMessages.sql', 'w+'),
-    'EventTypes'        : open('build/DB-build-EventTypes.sql', 'w+'),
-    'Events'            : open('build/DB-build-Events.sql', 'w+'),
-    'Links'             : open('build/DB-build-Links.sql', 'w+'),
-    'EventLinkLookUp'   : open('build/DB-build-EventLinkLookUp.sql', 'w+'),
-    'DeviceModels'      : open('build/DB-build-DeviceModels.sql', 'w+'),
-    'Devices'           : open('build/DB-build-Devices.sql', 'w+'),
-    'Purchases'         : open('build/DB-build-Purchases.sql', 'w+'),
-    'Registrations'     : open('build/DB-build-Registrations.sql', 'w+')
-}
-
-# Initialize files with "INSERT INTO TableName VALUES"
-for f in files.keys():
-    init(f)
+# Open files and initialize each table
+for t in tables:
+    files[t] = open('build/DB-build-%s.sql' % t, 'w+')
+    files[t].write('INSERT INTO %s VALUES\n' % t)
+    pk[t] = set()
 
 # Parse the CSV files provided by customer
 parse('data/CP_Account.csv', handle_account)
